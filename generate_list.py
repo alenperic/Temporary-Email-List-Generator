aa2fa2a90b79d07b5a9edf697f0b587d4d748cb3
FullSteeI <alenperic.art@gmail.com>
2023-10-25 11:40:42 -0400

    Update generate_list.py
    Added two output files for statistics.

--- generate_list.py
+++ generate_list.py
@@ -8,7 +8,6 @@ import urllib.error
 parser = argparse.ArgumentParser(description='Process CSV files.')
 parser.add_argument('-L', '--local-file', help='Specify a local file to use', required=False, default='old_list.csv')
 parser.add_argument('-C', '--column', help='Specify the column to use', required=False, default=0, type=int)
-parser.add_argument('-D', '--github-file', help='Specify a local .txt file containing GitHub repos', required=False)
 parser.add_argument('-S', '--sources-file', help='Specify a local .txt file containing data source URLs', required=False, default='sources.txt')
 parser.add_argument('-M', '--main-file', help='Specify the main CSV file', required=False, default='new_list_unedited.csv')
 parser.add_argument('-W', '--whitelist-file', help='Specify the whitelist CSV file', required=False, default='allowlist.csv')
@@ -21,10 +20,7 @@ def download_data_from_sources(file_path=None):
         with open(file_path, 'r') as f:
             urls = f.readlines()
     else:
-        urls = [
-            'http://example.com/pre-specified-file1.csv',
-            'http://example.com/pre-specified-file2.csv'
-        ]
+        urls = ['http://example.com/pre-specified-file1.csv', 'http://example.com/pre-specified-file2.csv']
 
     for url in urls:
         url = url.strip()
@@ -32,7 +28,6 @@ def download_data_from_sources(file_path=None):
             response = urllib.request.urlopen(url)
             if response.status == 200:
                 print(f"Source {url} is accessible.")
-                print(f"Downloading data from {url}...")
                 temp_df = pd.read_csv(url, header=None)
                 appended_data = pd.concat([appended_data, temp_df], ignore_index=True)
             else:
@@ -56,6 +51,7 @@ def remove_whitelisted(df, whitelist_path, column):
     whitelist_df = pd.read_csv(whitelist_path, header=None)
     removed_entries = df[df[column].isin(whitelist_df[column])]
     if not removed_entries.empty:
+        removed_entries.to_csv('allowlist_removed.txt', index=False, header=False, sep='\t')
         print("The following whitelist entries were removed:")
         print(removed_entries)
     else:
@@ -72,6 +68,17 @@ def compare_and_count_new_entries(df, past_df, column):
     new_entries = df[~df[column].isin(past_df[column])]
     return new_entries, len(new_entries)
 
+# Generate Output.txt
+def generate_output_txt(old_count, new_entries_count, new_list_count):
+    addition_total = old_count + new_entries_count
+    discrepancy = new_list_count - addition_total
+    with open('Output.txt', 'w') as f:
+        f.write(f'Old list entries: {old_count}\n')
+        f.write(f'New entries: {new_entries_count}\n')
+        f.write(f'New list: {new_list_count}\n')
+        f.write(f'Addition total: {addition_total}\n')
+        f.write(f'Discrepancy: {discrepancy}\n')
+
 if __name__ == "__main__":
     main_file = args.main_file
     whitelist_file = args.whitelist_file
@@ -80,7 +87,6 @@ if __name__ == "__main__":
     new_entries_file = "new_entries.csv"
     column = args.column
 
-    # Download data from sources if -S option is provided or use pre-specified URLs
     appended_data = None
     if args.sources_file:
         if os.path.exists(args.sources_file):
@@ -92,10 +98,7 @@ if __name__ == "__main__":
         print("Downloading data from pre-specified URLs...")
         appended_data = download_data_from_sources()
 
-    # Import the main CSV file
     main_df = import_csv(main_file)
-
-    # If downloaded data is available, append it to the main DataFrame
     if appended_data is not None and not appended_data.empty:
         main_df = pd.concat([main_df, appended_data], ignore_index=True)
 
@@ -107,3 +110,11 @@ if __name__ == "__main__":
     new_entries, count_new_entries = compare_and_count_new_entries(main_df, past_df, column)
     print(f"Number of new entries: {count_new_entries}")
     save_csv(new_entries, new_entries_file)
+
+    # Get the counts for each list
+    old_count = len(past_df)
+    new_entries_count = count_new_entries
+    new_list_count = len(main_df)
+
+    # Generate Output.txt with the counts and calculations
+    generate_output_txt(old_count, new_entries_count, new_list_count)
