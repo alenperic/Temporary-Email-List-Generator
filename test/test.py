bcbd5ab97a902bdeadc3e69deb46b0697902e0fa
FullSteeI <alenperic.art@gmail.com>
2023-10-24 23:36:25 -0400

    Add files via upload

---
+++ test/test.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import argparse
+import os
+
+# Command-line argument parsing
+parser = argparse.ArgumentParser(description='Process CSV files.')
+parser.add_argument('-L', '--local-file', help='Specify a local file to use', required=False)
+parser.add_argument('-C', '--column', help='Specify the column to use', required=False, default=0, type=int)
+parser.add_argument('-D', '--github-file', help='Specify a local .txt file containing GitHub repos', required=False, default='sources.txt')
+parser.add_argument('-M', '--main-file', help='Specify the main CSV file', required=False, default='new_list_unedited.csv')
+parser.add_argument('-W', '--whitelist-file', help='Specify the whitelist CSV file', required=False, default='allowlist.csv')
+args = parser.parse_args()
+
+# Fetch data from GitHub repositories specified in a .txt file
+# (placeholder; actual implementation depends on what data you need from GitHub)
+def fetch_data_from_github(file_path):
+    with open(file_path, 'r') as f:
+        repos = f.readlines()
+    for repo in repos:
+        repo = repo.strip()
+        print(f"Fetching data from GitHub repo: {repo}")
+        # Implement your code to fetch and append data from each GitHub repository here
+    return None  # Return appended DataFrame
+
+# Import the main CSV file
+def import_csv(file_path):
+    df = pd.read_csv(file_path, header=None)
+    return df
+
+# De-duplicate the entries
+def deduplicate(df, column):
+    return df.drop_duplicates(subset=[column])
+
+# Remove entries that are in the whitelist
+def remove_whitelisted(df, whitelist_path, column):
+    whitelist_df = pd.read_csv(whitelist_path, header=None)
+    df = df[~df[column].isin(whitelist_df[column])]
+    return df
+
+# Save as a CSV file
+def save_csv(df, file_path):
+    df.to_csv(file_path, index=False, header=False)
+
+# Compare to a past list and count new entries
+def compare_and_count_new_entries(df, past_df, column):
+    new_entries = df[~df[column].isin(past_df[column])]
+    return new_entries, len(new_entries)
+
+if __name__ == "__main__":
+    # File paths
+    main_file = args.main_file
+    whitelist_file = args.whitelist_file
+    past_file = args.local_file if args.local_file else "old_list.csv"
+    output_file = "new_list.csv"
+    new_entries_file = "new_entries.csv"
+    column = args.column
+
+    # Fetch data from GitHub if -D option is provided
+    if args.github_file:
+        if os.path.exists(args.github_file):
+            print(f"Fetching data from GitHub repositories specified in {args.github_file}...")
+            github_data = fetch_data_from_github(args.github_file)
+            # Implement code to append or merge GitHub data with main_df
+        else:
+            print(f"Error: File {args.github_file} does not exist.")
+
+    # Import the main CSV file
+    main_df = import_csv(main_file)
+
+    # De-duplicate the entries
+    main_df = deduplicate(main_df, column)
+
+    # Remove entries that are in the whitelist
+    main_df = remove_whitelisted(main_df, whitelist_file, column)
+
+    # Save as a CSV file
+    save_csv(main_df, output_file)
+
+    # Compare to a past list and count new entries
+    past_df = import_csv(past_file)
+    new_entries, count_new_entries = compare_and_count_new_entries(main_df, past_df, column)
+    print(f"Number of new entries: {count_new_entries}")
+
+    # Save the new entries as a CSV file
+    save_csv(new_entries, new_entries_file)
